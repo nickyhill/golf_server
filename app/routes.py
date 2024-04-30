57afd5e640dff668e2b03c7eb216dea22a0a1904
Nicholas Hillengas <nhillengas@vt.edu>
2024-04-30 14:12:55 -0400

    updating repo

--- app/routes.py
+++ app/routes.py
@@ -63,7 +63,7 @@ def leaderboard():
 @app.route('/player/<int:player_id>')
 def player_detail(player_id):
     players = DatabaseAPI.get_players_name()
-    player_info = DatabaseAPI.get_player_info(player_id)  # Update this method based on your API
+    player_info = DatabaseAPI.get_player_info(player_id)
     player_matches = DatabaseAPI.get_specific_player_matches(player_id)
     print(player_matches)
     return render_template('player_detail.html',
